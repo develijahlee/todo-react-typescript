10d6fac45e67b649a0bb9eae2b25576ae74a3f88
Elijah Lee <develijahlee@gmail.com>
2020-12-06 14:39:50 +0900

    finish part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { TodoListItem } from './TodoListItem';
 
-function App() {
-  return <div>Hello World</div>;
-}
+const todos: Array<Todo> = [
+  { text: 'Walk the dog', complete: true },
+  { text: 'Write app', complete: false },
+];
+
+const App: React.FC = () => {
+  return (
+    <>
+      <TodoListItem todo={todos[0]} />
+      <TodoListItem todo={todos[1]} />
+    </>
+  );
+};
 
 export default App;
